7b141ec685e3f2c411e07fa9f8eb1bc79dee58b5
IzmaylovAndrey <whiteshaq41@gmail.com>
2016-11-17 01:29:00 +0300

    Create Django models and make initial migration to DB

---
+++ cafe/data_management/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Example: soup, drink, etc.
+class TypeOfDish(models.Model):
+    type = models.TextField(max_length=30)
+
+
+class OrderStatus(models.Model):
+    status = models.TextField(max_length=20)
+
+
+class Role(models.Model):
+    role_name = models.TextField(max_length=20)
+
+
+class Worker(models.Model):
+    account = models.ForeignKey(User)
+    first_name = models.TextField(max_length=32)
+    last_name = models.TextField(max_length=32)
+    role = models.ForeignKey(Role)
+
+
+# Create your models here.
+class Dish(models.Model):
+    title = models.TextField(max_length=128, default='')
+    weight = models.FloatField(default=0.0)
+    ingredients = models.TextField()
+    price = models.FloatField(default=0.0)
+    type = models.ForeignKey(TypeOfDish)
+
+
+# Ready, Steady, GO
+class StateOfOrderItem(models.Model):
+    state = models.TextField(max_length=20)
+
+
+class OrderItem(models.Model):
+    state = models.ForeignKey(StateOfOrderItem)
+    amount = models.SmallIntegerField(null=False, default=1)
+    comment = models.TextField(max_length=255, null=True)
+    cooker = models.ForeignKey(Worker)
+
+
+class Order(models.Model):
+    waiter = models.ForeignKey(Worker)
+    status = models.ForeignKey(OrderStatus)
+    price = models.FloatField(default=0.0,)
